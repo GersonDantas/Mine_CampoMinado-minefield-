3d98f59d29573956afa340a5f7cb650acfc1592b
GersonDantas <gerson.accari@hotmail.com>
2020-04-30 20:22:44 -0300

    colocação de nivel do jogo e finalização do jogo

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {StyleSheet, View, Text, Alert} from 'react-native';
 import params from './src/params';
 import MineField from './src/components/MineField';
 import Header from './src/components/Header';
+import LevelSelection from './src/screen/Modal';
 import {
   createMinedBoard,
   cloneBoard,
@@ -33,6 +34,9 @@ export default class App extends Component {
     const rows = params.getRowAmount() - 1;
     return {
       board: createMinedBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false,
+      showLevelSelection: false,
     };
   };
 
@@ -66,13 +70,24 @@ export default class App extends Component {
     this.setState({board, wow});
   };
 
+  onLevelSelected = level => {
+    params.difficultLevel = level;
+    this.setState(this.createState());
+  };
+
   render() {
     return (
       <>
         <View style={styles.container}>
+          <LevelSelection
+            isVisible={this.state.showLevelSelection}
+            onLevelSelected={this.onLevelSelected}
+            onCancel={() => this.setState({showLevelSelection: false})}
+          />
           <Header
             flagsLeft={this.minesAmount() - flagUsed(this.state.board)}
             onNewGame={() => this.setState(this.createState())}
+            onFlagPress={() => this.setState({showLevelSelection: true})}
           />
           <View style={styles.board}>
             <MineField
